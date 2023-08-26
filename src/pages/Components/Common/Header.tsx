09f3ff82dc0aae3f5539f129855577662c3dd73a
anubhav-aryan <aryananubhav08@gmail.com>
2023-08-27 01:06:49 +0530

    fix: added responsivness to header

--- src/pages/Components/Common/Header.tsx
+++ src/pages/Components/Common/Header.tsx
@@ -6,37 +6,72 @@ const Header = () => {
   return (
     <>
       <div>
-        <header className="text-white bg-gray-900 body-font">
-          <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <nav className="bg-gray-900 w-full z-20 top-0 left-0 pb-16">
+          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <Link href="/">
-              <Image
-                src="/logo.webp"
-                alt="Picture of the author"
-                width={100}
-                height={100}
-              />
+    
+              <Image src="/logocoreship.png" width={100} height={100} alt="" />
+            
             </Link>
-            <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center gap-80">
-              <Link
-                href=" "
-                className="mr-5 text-2xl font-semibold hover:text-red-600"
+            <div className="flex md:order-2">
+              <Link href="/">
+              <button
+                type="button"
+                className="text-white bg-blue-700 hover:bg-blue-800 px-4 py-2 rounded-md"
               >
-                Courses
-              </Link>
-              <Link
-                href=" "
-                className="mr-5 text-2xl font-semibold hover:text-red-600"
-              >
-                Features
-              </Link>
-            </nav>
-            <Link href="https://docs.google.com/forms/d/e/1FAIpQLSd-njbuH5Wcc45y_CDr_zRNz1RUZvWJhikAyEVwpnD1ckksaw/viewform">
-              <button className="inline-flex items-center text-xl bg-gray-800 border-2 rounded-xl border-white py-1 px-3 focus:outline-none hover:bg-gray-700 mt-4 md:mt-0">
                 Register
               </button>
-            </Link>
+              <button
+                data-collapse-toggle=""
+                type="button"
+                className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                aria-controls=""
+                aria-expanded="true"
+              >
+                <span className="sr-only">Open main menu</span>
+                <svg
+                  className="w-5 h-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 17 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M1 1h15M1 7h15M1 13h15"
+                  />
+                </svg>
+              </button>
+              </Link>
+            </div>
+            <div
+              className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+              id=""
+            >
+              <ul className="gap-60 flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <li>
+                  <a
+                    href=""
+                    className="text-2xl py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  >
+                    Courses
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href=" "
+                    className="text-2xl block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  >
+                    Features
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
-        </header>
+        </nav>
       </div>
     </>
   );
