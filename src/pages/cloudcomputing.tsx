d99d54f048692de73dccfc20df6f17bf6fb89da8
anubhav-aryan <aryananubhav08@gmail.com>
2023-08-26 15:26:43 +0530

    feat: added cloud computing page

---
+++ src/pages/cloudcomputing.tsx
@@ -0,0 +1,334 @@
+import React from "react";
+import Header from "./Components/Common/Header";
+import Footer from "./Components/Common/Footer";
+
+const cloudcomputing = () => {
+  return (
+    <div>
+      <Header />
+      <section className="text-gray-400 bg-gray-900 body-font overflow-hidden">
+        <div className="container px-5 py-24 mx-auto bg-gray-900">
+          <div className="lg:w-4/5 mx-auto flex flex-wrap">
+            <img
+              alt="ecommerce"
+              className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
+              src="/cloudcomputing.svg"
+            />
+            <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+              <h2 className="text-sm title-font text-gray-500 tracking-widest">
+                Coreship{" "}
+              </h2>
+              <h1 className="text-white text-3xl title-font font-medium mb-1">
+                Cloud Computing
+              </h1>
+              <div className="flex mb-4">
+                <span className="flex items-center">
+                  <svg
+                    fill="currentColor"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-4 h-4 text-indigo-400"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                  </svg>
+                  <svg
+                    fill="currentColor"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-4 h-4 text-indigo-400"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                  </svg>
+                  <svg
+                    fill="currentColor"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-4 h-4 text-indigo-400"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                  </svg>
+                  <svg
+                    fill="currentColor"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-4 h-4 text-indigo-400"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                  </svg>
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-4 h-4 text-indigo-400"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                  </svg>
+                  <span className="ml-3">4 Reviews</span>
+                </span>
+              </div>
+              <p className="leading-relaxed">
+                Explore the dynamic world of cloud computing: Learn to harness
+                the power of distributed systems, storage solutions, and
+                scalable applications across major platforms.
+              </p>
+              <p className="leading-relaxed pt-2">
+                <span className="text-bold text-white">Timeline: </span> 3
+                months
+              </p>
+              <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-800 mb-5"></div>
+              <div className="flex">
+                <span className="title-font font-medium text-2xl text-white">
+                  Rs 1699
+                </span>
+                <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+                  Register
+                </button>
+                <button className="rounded-full w-10 h-10 bg-gray-800 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+                  <svg
+                    fill="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* ------------------------------------------Content--------------------------------------------------- */}
+      <div className="">
+        <section className="text-white bg-gray-900">
+          <div className="bg-gray-900 ">
+            <div className="container px-5 pb-24 mx-auto">
+              <div className="xl:w-1/2 lg:w-3/4 w-full mx-auto text-left border-solid border-white border-2 rounded-3xl px-14 py-14 bg-slate-700">
+                <p className="leading-relaxed text-3xl font-bold">
+                  Course Syllabus:{" "}
+                </p>
+                <p className="leading-relaxed text-2xl font-bold pt-6">
+                  Month 1: Introduction to Cloud Computing{" "}
+                </p>
+                <ul
+                  role="list"
+                  className="marker:text-sky-400 list-disc pl-5 space-y-3 text-slate-400 pt-8"
+                >
+                  <li>Understanding Cloud Computing Concepts</li>
+                  <li>
+                    What is Cloud Computing? Exploring the fundamental concepts
+                    and benefits of cloud computing.
+                  </li>
+                  <li>
+                    Cloud Service Models: IaaS, PaaS, SaaS. Exploring
+                    Infrastructure as a Service, Platform as a Service, and
+                    Software as a Service.
+                  </li>
+                  <li>
+                    Cloud Deployment Models: Public, Private, Hybrid, and
+                    Multi-Cloud. Understanding the different deployment options
+                    and their use cases.
+                  </li>
+                </ul>
+                <p className="leading-relaxed text-2xl font-bold pt-6">
+                  Projects:
+                </p>
+                <ul
+                  role="list"
+                  className="marker:text-sky-400 list-disc pl-5 space-y-3 text-slate-400 pt-8"
+                >
+                  <li>
+                    Setting up a Free Tier Account on AWS/Azure/GCP. Practical
+                    hands-on experience with creating an account on a major
+                    cloud provider and navigating the console.
+                  </li>
+                </ul>
+                <p className="leading-relaxed text-2xl font-bold pt-6">
+                  Month 2: Advanced Cloud Services and Solutions
+                </p>
+                <ul
+                  role="list"
+                  className="marker:text-sky-400 list-disc pl-5 space-y-3 text-slate-400 pt-8"
+                >
+                  <li>Serverless Computing</li>
+                  <li>
+                    Introduction to Serverless Architecture. Understanding the
+                    serverless paradigm and its advantages.
+                  </li>
+                  <li>
+                    AWS Lambda, Azure Functions, Google Cloud Functions.
+                    Exploring major serverless computing platforms
+                  </li>
+                  <li>
+                    Building Serverless Applications. Creating applications
+                    without managing servers.
+                  </li>
+                </ul>
+                <p className="leading-relaxed text-2xl font-bold pt-6">
+                  Projects
+                </p>
+                <ul
+                  role="list"
+                  className="marker:text-sky-400 list-disc pl-5 space-y-3 text-slate-400 pt-8"
+                >
+                  <li>
+                    Developing a Serverless API. Hands-on creation of a
+                    serverless API using AWS Lambda or similar platform.
+                  </li>
+                </ul>
+                <p className="leading-relaxed text-2xl font-bold pt-6">
+                  Month 3: Cloud Migration and DevOps
+                </p>
+                <ul
+                  role="list"
+                  className="marker:text-sky-400 list-disc pl-5 space-y-3 text-slate-400 pt-8"
+                >
+                  <li>Cloud Migration Strategies</li>
+                  <li>
+                    Planning and Assessing Migration. Steps involved in planning
+                    a successful cloud migration.
+                  </li>
+                  <li>
+                    Lift-and-Shift vs. Re-architecting. Comparing migration
+                    approaches.
+                  </li>
+                  <li>
+                    Cloud Migration Tools and Techniques. Exploring tools for
+                    seamless migration.
+                  </li>
+                  <li>
+                    Cloud Migration Tools and Techniques. Exploring tools for
+                    seamless migration.
+                  </li>
+                  <li>
+                    Project: Migrating an On-Premise Application to the Cloud.
+                    Practical migration of an application to a cloud
+                    environment.
+                  </li>
+                  <li>DevOps in the Cloud</li>
+                  <li>
+                    Introduction to DevOps Principles. Understanding the DevOps
+                    culture and practices.
+                  </li>
+                  <li>
+                    CI/CD Pipelines on the Cloud. Setting up continuous
+                    integration and continuous deployment pipelines.
+                  </li>
+                  <li>
+                    Infrastructure as Code (IaC) with Terraform/CloudFormation.
+                    Automating infrastructure setup using code.
+                  </li>
+                  <li>
+                    Project: Setting up a CI/CD Pipeline for a Web Application.
+                    Creating an end-to-end pipeline for a web app.
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+
+      {/* -----------------------------------------Table---------------------------------------------------- */}
+      <div className="bg-gray-900 flex justify-center items-center pb-28">
+        <div className="container bg-gray-900 w-1/2 ">
+          <h2 className="text-3xl font-bold text-center text-white py-8">
+            Why Choose Us?
+          </h2>
+          <div className="">
+            <div className="grid grid-cols-2 p-4 text-sm font-medium text-gray-900 bg-gray-100 border-t border-b border-gray-200 gap-x-16 dark:bg-gray-800 dark:border-gray-700 dark:text-white">
+              <div className="flex items-center">Enrollment Benefits</div>
+              <div>OROM</div>
+            </div>
+            <div className="grid grid-cols-2 px-4 py-5 text-sm text-gray-700 border-b border-gray-200 gap-x-16 dark:border-gray-700">
+              <div className="text-gray-500 dark:text-gray-400">
+                Project Based Learning
+              </div>
+              <div>
+                <svg
+                  className="w-3 h-3 text-green-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 16 12"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M1 5.917 5.724 10.5 15 1.5"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 px-4 py-5 text-sm text-gray-700 border-b border-gray-200 gap-x-16 dark:border-gray-700">
+              <div className="text-gray-500 dark:text-gray-400">
+                Indutrial Level Experience
+              </div>
+              <div>
+                <svg
+                  className="w-3 h-3 text-green-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 16 12"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M1 5.917 5.724 10.5 15 1.5"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 px-4 py-5 text-sm text-gray-700 border-b border-gray-200 gap-x-16 dark:border-gray-700">
+              <div className="text-gray-500 dark:text-gray-400">
+                Job Ready Portfolio
+              </div>
+              <div>
+                <svg
+                  className="w-3 h-3 text-green-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 16 12"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M1 5.917 5.724 10.5 15 1.5"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default cloudcomputing;
