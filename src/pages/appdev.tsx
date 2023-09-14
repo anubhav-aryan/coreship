71822cec58a545de1997ef0205f4bdfe11d929ca
anubhav-aryan <aryananubhav08@gmail.com>
2023-09-14 21:47:59 +0530

    fix: payment link

--- src/pages/appdev.tsx
+++ src/pages/appdev.tsx
@@ -99,7 +99,7 @@ const appdevelopment = () => {
                 <div className="ml-auto">
                   <Link
                     href={
-                      "https://checkout.stripe.com/c/pay/cs_live_a1vBntFI5onVIRDuiowoZYf0wJFvSdXO6eqR4AAaQXL7XEw2CNwoT4x3xU#fidkdWxOYHwnPyd1blppbHNgWjA0S3VEZkhWQkhETnEyUWo1VFV%2FbV1QN2pHbX18UGt1XUlVPXJQUldUNVxDXzVJV2lvRmJTfVZUQzc1Nl1EPWhTREBWaTVTVVN%2FV098VUh0cTRmU2F0RE1DNTUzYUthMldjPScpJ3VpbGtuQH11anZgYUxhJz8nY19gMGlDMENiZFIyY29SYVRUJ3gl"
+                      "https://checkout.stripe.com/c/pay/cs_live_a1xZ6t8o0TKs8WeHnH1U7bWdqaWUzGquKH9KzVwcS5G2cUl03DOTX0p4l9#fidkdWxOYHwnPyd1blppbHNgWjA0S3VEZkhWQkhETnEyUWo1VFV%2FbV1QN2pHbX18UGt1XUlVPXJQUldUNVxDXzVJV2lvRmJTfVZUQzc1Nl1EPWhTREBWaTVTVVN%2FV098VUh0cTRmU2F0RE1DNTUzYUthMldjPScpJ3VpbGtuQH11anZgYUxhJz8nY19gMGlDMENiZFIyY29SYVRUJ3gl"
                     }
                   >
                     <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
