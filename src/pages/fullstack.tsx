a096ca804d0ee0f9e3030aaa3a6797f28bb89812
anubhav-aryan <aryananubhav08@gmail.com>
2023-10-02 22:06:56 +0530

    fix: image tags

--- src/pages/fullstack.tsx
+++ src/pages/fullstack.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Components/Common/Header";
 import Footer from "./Components/Common/Footer";
 import Link from "next/link";
+import Image from "next/image";
 
 const fullstack = () => {
   return (
@@ -10,11 +11,7 @@ const fullstack = () => {
       <section className="text-gray-400 bg-gray-900 body-font overflow-hidden">
         <div className="container px-5 py-24 mx-auto bg-gray-900">
           <div className="lg:w-4/5 mx-auto flex flex-wrap">
-            <img
-              alt="ecommerce"
-              className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
-              src="/software-engineer.svg"
-            />
+            <Image src="/software-engineer.svg" width={500} height={500} alt=" " className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" />
             <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
               <h2 className="text-sm title-font text-gray-500 tracking-widest">
                 Coreship{" "}
